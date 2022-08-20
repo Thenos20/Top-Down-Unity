06bf8d59c3a6bf1f9ee8b46c54cc629cf25e798c
Thenos20 <ask.theo.ta@gmail.com>
2022-08-20 22:58:42 +0200

    Update EnemySpawner.cs

--- Top-down/Assets/Scripts/EnemySpawner.cs
+++ Top-down/Assets/Scripts/EnemySpawner.cs
@@ -14,7 +14,7 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (SlimePrefab.activeSelf == true){
+        if (SlimePrefab.activeSelf == false){
             StartCoroutine(spawnEnemy(slimeInterval, SlimePrefab));
         }
     }
