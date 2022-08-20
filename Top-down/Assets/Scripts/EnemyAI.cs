bb77e22251845f4399c091450a15563b87c55c56
Thenos20 <ask.theo.ta@gmail.com>
2022-08-20 19:35:05 +0200

    New Scene, "Enemy_Spawn_Land"

--- Top-down/Assets/Scripts/EnemyAI.cs
+++ Top-down/Assets/Scripts/EnemyAI.cs
@@ -7,9 +7,16 @@ public class EnemyAI : MonoBehaviour
     public float AIspeed = 1f;
     private Transform target;
     Animator animator;
+    public GameObject playertofind;
+
+    SpriteRenderer spriteRenderer;
+
 
     private void Start() {
         animator = GetComponent<Animator>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     public void EnemyMove(){
@@ -21,6 +28,22 @@ public class EnemyAI : MonoBehaviour
     }
 
     private void Update(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        // Get direction from A to B
+        Vector3 posA = playertofind.transform.position;
+        Vector3 posB = transform.position;
+        //Destination - Origin
+        Vector3 dir = (posB - posA).normalized;
+        
+        if(dir.x > 0) {
+                spriteRenderer.flipX = true;
+            } else if (dir.x < 0) {
+                spriteRenderer.flipX = false;
+            }
+
+
+
+
         if (target != null){
             float step = AIspeed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, target.position, step);
@@ -31,6 +54,7 @@ public class EnemyAI : MonoBehaviour
 
             EnemyStopMove();
         }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other){
@@ -45,5 +69,10 @@ public class EnemyAI : MonoBehaviour
             target = null;
         }
     }
+
+
+
+    
+    
 
 }
